0810677e0eca381ff20165df9f0eaa9804b7fd3d
Divyam G <dgarg1@umd.edu>
2020-11-24 15:05:48 -0500

    Removed unnecessary files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/divyam/turtlebot_walker/src
-# Build directory: /home/divyam/turtlebot_walker/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(walker)
