73b0b672a1a9a37a80471e7e54b939417c03659e
Divyam G <dgarg1@umd.edu>
2020-11-24 01:40:04 -0500

    [walker.hpp] Added walker class header file

---
+++ src/walker/include/walker.hpp
@@ -0,0 +1,15 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/LaserScan.h>
+#include <std_msgs/String.h>
+
+class Walker {
+private:
+	geometry_msgs::Twist twist;
+	ros::Publisher move_pub;
+	ros::Subscriber laser_sub;
+public:
+	Walker(ros::NodeHandle n);
+	~Walker();
+	void laserCallBack(const sensor_msgs::LaserScan::ConstPtr& scan);
+};
