7bead4f548aff8d5c7d5cc63b3525591cd17fd0b
Divyam G <dgarg1@umd.edu>
2020-11-24 14:41:44 -0500

    Added doxygen commenting

--- src/walker/include/walker.hpp
+++ src/walker/include/walker.hpp
@@ -1,19 +1,69 @@
+/**
+ * @file walker.hpp : This is a header file which initialises Walker class to
+ * avoid global variable while publishing
+ * @author Divyam Garg (dgarg1@umd.edu)
+ * @brief
+ * @version 0.1
+ * @date 2020-11-22
+ * @license MIT License
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+/**
+ * this behaves as pragma once
+ */
+
 #ifndef INCLUDE_WALKER_HPP_
 #define INCLUDE_WALKER_HPP_
-
-#include <ros/ros.h>
+/**
+ * Ros headers and message headers
+ */
 #include <geometry_msgs/Twist.h>
+#include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <std_msgs/String.h>
 
 class Walker {
 private:
-	geometry_msgs::Twist twist;
-	ros::Publisher move_pub;
-	ros::Subscriber laser_sub;
+  geometry_msgs::Twist twist;
+  ros::Publisher move_pub;
+  ros::Subscriber laser_sub;
+
 public:
-	Walker(ros::NodeHandle n);
-	~Walker();
-	void laserCallBack(const sensor_msgs::LaserScan::ConstPtr& scan);
+  /**
+   * @brief Construct a new Walker object
+   *
+   * @param n
+   */
+  Walker(ros::NodeHandle n);
+  /**
+   * @brief Destroy the Walker object
+   *
+   */
+  ~Walker();
+  /**
+   * @brief callback function to read scan messages
+   *
+   * @param scan
+   */
+  void laserCallBack(const sensor_msgs::LaserScan::ConstPtr &scan);
 };
 #endif // include Walker.hpp
--- src/walker/scripts/walker.cpp
+++ src/walker/scripts/walker.cpp
@@ -1,34 +1,81 @@
+/**
+ * @file walker.cpp : This files initialises and executes the ros node named
+ * walker
+ * @author Divyam Garg (dgarg1@umd.edu)
+ * @brief
+ * @version 0.1
+ * @date 2020-11-22
+ * @license MIT License
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+
 #include "../include/walker.hpp"
 
+/**
+ * @brief Construct a new Walker:: Walker object initialises twist message and
+ * publish and subscribe topics
+ *
+ * @param n
+ */
 Walker::Walker(ros::NodeHandle n)
- : move_pub(n.advertise<geometry_msgs::Twist>("/cmd_vel",10)),
-   laser_sub(n.subscribe("/scan", 10, &Walker::laserCallBack, this)){
-   	twist.linear.x = 0;
-   	twist.linear.z = 0;
-   	move_pub.publish(twist);
-   	ROS_INFO("Walker node initialized");
-   };
+    : move_pub(n.advertise<geometry_msgs::Twist>("/cmd_vel", 10)),
+      laser_sub(n.subscribe("/scan", 10, &Walker::laserCallBack, this)) {
+  twist.linear.x = 0;
+  twist.linear.z = 0;
+  move_pub.publish(twist);
+  ROS_INFO("Walker node initialized");
+};
+/**
+ * @brief Destroy the Walker:: Walker object
+ *
+ */
+Walker::~Walker() { ROS_INFO("Walker_node is shutting down"); };
+/**
+ * @brief callback function that waits for scan message and accordingly
+ * publishes the velocity
+ *
+ * @param scan
+ */
+void Walker::laserCallBack(const sensor_msgs::LaserScan::ConstPtr &scan) {
+  bool flag_check = false;
+  // checking at -30, 0 and 30 degrees for object at a certain threshold value
+  if (scan->ranges[0] <= 0.6 || scan->ranges[30] <= 0.6 ||
+      scan->ranges[330] <= 0.6) {
+    flag_check = true;
+  };
 
- Walker::~Walker(){
- 	ROS_INFO("Walker_node is shutting down");
- };
-
- void Walker::laserCallBack(const sensor_msgs::LaserScan::ConstPtr &scan){
- 	bool flag_check = false;
- 	if (scan->ranges[0] <= 0.6 || scan->ranges[30] <= 0.6 || scan->ranges[330] <= 0.6){
- 		flag_check = true;
- 	};
-
- 	if (flag_check){
- 		twist.linear.x = 0;
- 		twist.angular.z = 0.8;
- 		move_pub.publish(twist);
- 		ROS_WARN("Obstacle ahead, taking a turn");
- 	}
- 	if (flag_check == false) {
- 		twist.linear.x = 0.5;
- 		twist.angular.z = 0.0;
- 		move_pub.publish(twist);
- 		ROS_INFO("No Obstacle ahead, moving forward");
- 	};
- };
+  // publish angular velocity if the obstacle is within threshold value
+  if (flag_check) {
+    twist.linear.x = 0;
+    twist.angular.z = 0.8;
+    move_pub.publish(twist);
+    ROS_WARN("Obstacle ahead, taking a turn");
+  }
+  // publish linear velocity if the obstacle is not within threshold value
+  if (flag_check == false) {
+    twist.linear.x = 0.5;
+    twist.angular.z = 0.0;
+    move_pub.publish(twist);
+    ROS_INFO("No Obstacle ahead, moving forward");
+  };
+};
